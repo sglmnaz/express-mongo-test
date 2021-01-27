22e4ace48375bab3078d51ba4a9d864da0c76fa6
daniele <stoughtonrich@gmail.com>
2021-01-27 16:31:46 +0000

    fix

--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk'
 import { MongoClient } from 'mongodb'
 
 export const connectToDb = (url: any, dbName: any) => {
@@ -7,7 +8,7 @@ export const connectToDb = (url: any, dbName: any) => {
         // Use connect method to connect to the server
         MongoClient.connect(url , { useUnifiedTopology: true })
             .then((client) => {
-                console.log('Connected to Database')
+                console.log('Connected to Database: ' + chalk.blue(dbName))
                 resolve(client.db(dbName))
             })
             .catch((error) => {
